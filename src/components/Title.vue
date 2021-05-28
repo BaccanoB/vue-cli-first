2ad5959353d1c1b44b0d2239629f9ea640f9d00d
BaccanoB <beatrice.baccano@yahoo.it>
2021-05-28 16:57:00 +0200

    Aggiunto Title.vue e collegato correttamente!

---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1> La mia prima app con Vue CLI! </h1>
+</template>
+
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "Title"
+  }
+</script>
+
+<style>
+    h1 {
+        font-size: 40px;
+    }
+</style>
